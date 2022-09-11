6590d99eb4cfa9d6249618742a7a62662a761364
be-jojo <67816573+be-jojo@users.noreply.github.com>
2022-09-11 19:56:41 +0530

    Delete Header.js

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Header() {
-    return (
-        <header className='header'>
-            <h3 className='header-title'>Sorting Visualizer</h3>
-        </header>
-    );
-};
